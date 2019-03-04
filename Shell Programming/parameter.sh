69afe9ee36642c2aa1f941bda47bd1ab886447d5
Amrita <theamritanair@gmail.com>
2019-03-04 11:42:14 +0530

    Access the arguements in commandline

---
+++ Shell Programming/parameter.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# To access the command line arguements, you have to add a $ followed by the arguement number.
+# For instance, $1 gives the first arguement and so on.
+# To get the total number of arguements, $#
+
+
+echo "The filename is $0"
+echo "The first parameter is $1"
+echo "The total number of parameters are : $#"
