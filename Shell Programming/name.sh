ff1121c500503b4cf2d13cd15f67cecdcf9703f7
Amrita <theamritanair@gmail.com>
2019-03-04 11:39:22 +0530

    Prints the input from the command line

--- Shell Programming/name.sh
+++ Shell Programming/name.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 #Author: Amrita Nair
+#The read command takes input from the command line and stores it in a variable.
+#To access the variable, you have to use the $ sign before the variable name.
 
 echo "What is your name?"
 read name
